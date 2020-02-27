3625aa226f5d3b7f8cfb95857ebf44145ccd195e
skcodehub <44783634+skcodehub@users.noreply.github.com>
2020-02-27 14:17:56 -0800

    Update load_dimension.py with comments
    added comments

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -1,7 +1,11 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
-
+"""
+LoadDimensionOperator loads dimension tables to Redshift based on the query provided
+    redshift connection
+    insert statement for fact table
+"""
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
